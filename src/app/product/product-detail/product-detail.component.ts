eaa5fb5f35e0a5252fce0cc4ebec975b1002588b
Jeong Eunwoo <zenic88@naver.com>
2017-08-26 20:58:46 +0900

    상세보기 화면 커멘트 입력 기능 추가

--- src/app/product/product-detail/product-detail.component.ts
+++ src/app/product/product-detail/product-detail.component.ts
@@ -11,6 +11,11 @@ export class ProductDetailComponent implements OnInit {
   product: Product;
   reviews: Review[];
 
+  newComment: string;
+  newRating: number;
+
+  isReviewHidden = true;
+
   constructor(
     route: ActivatedRoute,
     productService: ProductService
@@ -21,6 +26,28 @@ export class ProductDetailComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  addReview () {
+    const review = new Review(0, this.product.id, new Date(), 'Anonymous',
+    this.newRating, this.newComment);
+    console.log('Adding review ' + JSON.stringify(review));
+
+    this.reviews = [...this.reviews, review];
+    this.product.rating = this.averageRating(this.reviews);
+
+    this.resetForm();
+  }
+
+  averageRating (reviews: Review[]) {
+    const sum = reviews.reduce((average, review) => average + review.rating, 0);
+    return sum / reviews.length;
+  }
+
+  resetForm () {
+    this.newRating = 0;
+    this.newComment = null;
+    this.isReviewHidden = true;
   }
 
 }
